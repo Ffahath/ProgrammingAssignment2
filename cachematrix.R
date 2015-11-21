639b7e7ef41b2f35079188fb52d4afddffd86f9b
Ffahath <Ffahath@gmail.com>
2015-11-21 18:25:48 +0100

    solution for the 2nd assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,83 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix() and cacheSolve() work together to enable
+## caching of results (matrix inverse) instead of calculating
+## the matrix inverse over and over again
 
-## Write a short comment describing this function
+## This function provides means to use a cached version
+## of matrix inverse, if available.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cached_inverse <- NULL
+    
+    # set the matrix and reset the cached inverse 
+    set <- function(y) {
+        x <<- y
+        cached_inverse <<- NULL
+    }
+    
+    # retrieve the current matrix
+    get <- function() x
+    
+    # store the inverse into the cached variable
+    setinverse <- function(inverse) cached_inverse <<- inverse
+    
+    # retrieve the cached inverse result
+    getinverse <- function() cached_inverse
+    
+    # return prepared caching object
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # retrieve the cached result from the caching object
+    inv <- x$getinverse()
+    
+    # no need to compute the inverse, if the cache result is ready
+    if(!is.null(inv)) {
+        message("cached data retrieved")
+        return(inv)
+    }
+    
+    data <- x$get()
+    # compute the inverse matrix
+    inv <- solve(data)
+    # store the inverse matrix into the caching object
+    x$setinverse(inv)
+    inv
+}
+
+## small test to check the correct functionality
+test <- function()
+{
+    A = matrix(c(2, 4, 3, 1), nrow=2, ncol=2) 
+    identityM = matrix(c(1, 0, 0, 1), nrow=2, ncol=2) 
+    
+    env = makeCacheMatrix(A)
+    
+    invA1 = cacheSolve(env)
+    print(invA1)
+    invA2 = cacheSolve(env)
+
+    if (!all.equal(A %*% invA2, identityM)) {
+        message("Incorrect result!")
+    }
+    
+    B = matrix(c(4, 2, 2, 3), nrow=2, ncol=2) 
+    # switch the background matrix
+    env$set(B)
+    
+    invB1 = cacheSolve(env)
+    print(invB1)
+    if (!all.equal(B %*% invB1, identityM)) {
+        message("Incorrect result!")
+    }
+    invB2 = cacheSolve(env)
+    invB2 = cacheSolve(env)
+    
 }
